84e6354f4481bf0ce20781a3ce178d8fd2134472
Joschi <josua.krause@googlemail.com>
2011-08-19 18:45:22 +0200

    header test

--- test/jkit/io/csv/CSVTest.java
+++ test/jkit/io/csv/CSVTest.java
@@ -13,10 +13,6 @@ import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import jkit.io.csv.CSVContext;
-import jkit.io.csv.CSVHandler;
-import jkit.io.csv.CSVReader;
-
 import org.junit.Test;
 
 /**
@@ -35,13 +31,24 @@ public class CSVTest {
 
 		private final String content;
 
+		private final String row;
+
+		private final String col;
+
 		public Event(final EventType type) {
 			this(type, null);
 		}
 
 		public Event(final EventType type, final String content) {
+			this(type, content, null, null);
+		}
+
+		public Event(final EventType type, final String content,
+				final String row, final String col) {
 			this.type = type;
 			this.content = content;
+			this.row = row;
+			this.col = col;
 		}
 
 		@Override
@@ -57,6 +64,12 @@ public class CSVTest {
 			}
 			final Event e = (Event) obj;
 			if (e.type != type) {
+				return false;
+			}
+			if (row != null && e.row != null && !row.equals(e.row)) {
+				return false;
+			}
+			if (col != null && e.col != null && !col.equals(e.col)) {
 				return false;
 			}
 			if (content == null) {
@@ -108,32 +121,32 @@ public class CSVTest {
 
 		@Override
 		public void cell(final CSVContext ctx, final String content) {
-			events.add(new Event(CELL, content));
+			events.add(new Event(CELL, content, ctx.rowName(), ctx.colName()));
 		}
 
 		@Override
 		public void colTitle(final CSVContext ctx, final String title) {
-			events.add(new Event(COL, title));
+			events.add(new Event(COL, title, ctx.rowName(), ctx.colName()));
 		}
 
 		@Override
 		public void row(final CSVContext ctx) {
-			events.add(new Event(ROW, null));
+			events.add(new Event(ROW, null, ctx.rowName(), ctx.colName()));
 		}
 
 		@Override
 		public void rowTitle(final CSVContext ctx, final String title) {
-			events.add(new Event(ROW, title));
+			events.add(new Event(ROW, title, ctx.rowName(), ctx.colName()));
 		}
 
 		@Override
 		public void start(final CSVContext ctx) {
-			events.add(new Event(START, null));
+			events.add(new Event(START, null, ctx.rowName(), ctx.colName()));
 		}
 
 		@Override
 		public void end(final CSVContext ctx) {
-			events.add(new Event(END, null));
+			events.add(new Event(END, null, ctx.rowName(), ctx.colName()));
 		}
 
 	}
@@ -168,6 +181,41 @@ public class CSVTest {
 
 	private static final String strTest2 = "a\"b;c\"d;e\"f;\"gh\"";
 
+	private static final String strTest3 = "-;c1;\"c2\";c3\nr1;1;2;3\n\"r2\";4;5;\"6\"\n\"r3\";7;8;9\n";
+
+	private static final Event[] evTest3r = new Event[] { new Event(START),
+			new Event(ROW, "-"), new Event(CELL, "c1"), new Event(CELL, "c2"),
+			new Event(CELL, "c3"), new Event(ROW, "r1"),
+			new Event(CELL, "1", "r1", null), new Event(CELL, "2", "r1", null),
+			new Event(CELL, "3", "r1", null), new Event(ROW, "r2"),
+			new Event(CELL, "4", "r2", null), new Event(CELL, "5", "r2", null),
+			new Event(CELL, "6", "r2", null), new Event(ROW, "r3"),
+			new Event(CELL, "7", "r3", null), new Event(CELL, "8", "r3", null),
+			new Event(CELL, "9", "r3", null), new Event(END) };
+
+	private static final Event[] evTest3c = new Event[] { new Event(START),
+			new Event(COL, "-"), new Event(COL, "c1"), new Event(COL, "c2"),
+			new Event(COL, "c3"), new Event(ROW),
+			new Event(CELL, "r1", null, "c1"),
+			new Event(CELL, "1", null, "c2"), new Event(CELL, "2"),
+			new Event(CELL, "3", null, "c3"), new Event(ROW),
+			new Event(CELL, "r2", null, "c1"),
+			new Event(CELL, "4", null, "c2"), new Event(CELL, "5"),
+			new Event(CELL, "6", null, "c3"), new Event(ROW),
+			new Event(CELL, "r3", null, "c1"),
+			new Event(CELL, "7", null, "c2"), new Event(CELL, "8"),
+			new Event(CELL, "9", null, "c3"), new Event(END) };
+
+	private static final Event[] evTest3rc = new Event[] { new Event(START),
+			new Event(COL, "c1"), new Event(COL, "c2"), new Event(COL, "c3"),
+			new Event(ROW, "r1"), new Event(CELL, "1", "r1", "c1"),
+			new Event(CELL, "2", "r1", "c2"), new Event(CELL, "3", "r2", "c3"),
+			new Event(ROW, "r2"), new Event(CELL, "4", "r2", "c1"),
+			new Event(CELL, "5", "r2", "c2"), new Event(CELL, "6", "r2", "c3"),
+			new Event(ROW, "r3"), new Event(CELL, "7", "r3", "c1"),
+			new Event(CELL, "8", "r3", "c2"), new Event(CELL, "9", "r3", "c3"),
+			new Event(END) };
+
 	private static final Event[] evTest2 = new Event[] { new Event(START),
 			new Event(ROW), new Event(CELL, "a\"b"), new Event(CELL, "c\"d"),
 			new Event(CELL, "e\"f"), new Event(CELL, "gh"), new Event(END) };
@@ -195,6 +243,16 @@ public class CSVTest {
 	@Test
 	public void test2() throws Exception {
 		doTest(new CSVReader(), strTest2, evTest2);
+	}
+
+	public void test3() throws Exception {
+		final CSVReader csv = new CSVReader();
+		csv.setReadRowTitles(true);
+		doTest(csv, strTest3, evTest3r);
+		csv.setReadColTitles(true);
+		doTest(csv, strTest3, evTest3rc);
+		csv.setReadRowTitles(false);
+		doTest(csv, strTest3, evTest3c);
 	}
 
 }
